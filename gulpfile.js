8300844719929a5c1a6f0daa477221f3bade2908
Andrei <franko.8813@gmail.com>
2019-01-10 22:14:45 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//подключаем gulp
+var gulp = require('gulp');
+
+//создаем тестовую задачу с именем mytest
+gulp.task('mytest', function(){
+
+    //выводим в терминале фразу 'It works'
+    console.log('It works');
+});
